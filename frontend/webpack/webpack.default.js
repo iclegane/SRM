e094f6ecc62383edc38876c491ab93e5bd04dd38
Viktor <vvs-PathOfExile@yandex.ru>
2023-01-15 16:59:14 +0300

    add css loaders and change webpack config

--- frontend/webpack/webpack.default.js
+++ frontend/webpack/webpack.default.js
@@ -29,6 +29,10 @@ module.exports = {
                     },
                 }
             },
+            {
+                test: /\.s[ac]ss$/i,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
         ],
     },
     plugins: [
