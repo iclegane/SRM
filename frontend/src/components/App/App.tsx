55799caabb5dec8c314e930138ff13527e7584d5
Viktor <vvs-PathOfExile@yandex.ru>
2023-01-15 14:57:13 +0300

    add App component

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 
-const App: React.FC = () => {
-    return (
+const App: React.FC = () => (
         <div>
             App
         </div>
-    )
-}
+    );
 
 export default App;
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from "./components/App/App";
+import App from './components/App/App';
 
 const domNode = document.getElementById('root') as Element;
 const root = ReactDOM.createRoot(domNode);
