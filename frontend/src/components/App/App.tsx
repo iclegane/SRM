dc69835ea31d61d9542593f3c147be19e7ef286d
Viktor <vvs-PathOfExile@yandex.ru>
2023-01-19 16:11:54 +0300

    change style variables

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -3,11 +3,9 @@ import { Route, Routes } from 'react-router';
 import Promo from "../../pages/Pomo/Promo";
 
 const App: React.FC = () => (
-        <div>
-            <Routes>
-                <Route path="/" element={<Promo/>} />
-            </Routes>
-        </div>
+        <Routes>
+            <Route path="/" element={<Promo/>} />
+        </Routes>
     );
 
 export default App;
