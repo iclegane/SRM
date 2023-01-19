94164eecb9fb5d1f750bdd9363b82fe081a0182f
Viktor <vvs-PathOfExile@yandex.ru>
2023-01-19 21:16:52 +0300

    added test components page

---
+++ frontend/src/pages/Components/Components.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './index.scss';
+
+const Components: React.FC = () => {
+
+
+    return (
+        <div className={'page page--components'}>
+
+        </div>
+    )
+}
+
+export default Components;
